68d24a690b680da88502b91ddf892bf8c24d53ca
nakulshah <nakulshah@gmail.com>
2022-11-28 21:42:53 -0600

    secret number generated

--- RandomNumberGuess.py
+++ RandomNumberGuess.py
@@ -1,4 +1,6 @@
 # This is a Python script for random number guessing game.
+import random
+
 
 # Press Shift+F10 to execute it
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -7,6 +9,13 @@
 def play_game(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}. Let\'s play a game of guessing a random number')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Aim of the Game: '
+          f'The computer has chosen a random number between 1 to 100, your aim is to identify the number'
+          f' within 10 turns.')
+    print(f'The rules of the game are simple: '
+          f'')
+
+    secret_number = random.randint(1, 100)
 
 
 # Press the green button in the gutter to run the script.
