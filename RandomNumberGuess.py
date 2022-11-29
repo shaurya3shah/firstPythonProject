019d1e8cb43f27addd9cbf973a6e0454e7a3940a
nakulshah <nakulshah@gmail.com>
2022-11-28 21:15:58 -0600

    random number guessing game - start

---
+++ RandomNumberGuess.py
@@ -0,0 +1,15 @@
+# This is a Python script for random number guessing game.
+
+# Press Shift+F10 to execute it
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def play_game(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}. Let\'s play a game of guessing a random number')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    player_name = input("What is your name?")
+    play_game(player_name)
