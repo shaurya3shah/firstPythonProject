3774f402f21f2de77fe0c5aa24f83f2ec523c4ad
shaurya3shah <shaurya.shah@mage.education>
2022-11-28 20:28:26 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# first python program
+import math
+
+print("Hi, Shaurya")
+print("what is 1234567890 X 1234567890")
+print(math.pow(10*10, 10))
+
+user_input = input("99 * 3?")
+
+print(297)
+
+user_input = input("20^2")
+
+print(400)
+
+user_input = input("3rd perfect number")
+
+print(496)
+
+user_input = input("50^2")
+
+print(2500)
+
+user_input = input("true or false  ")
+
+print(2500)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
